bac4c6e74a7481ad55cd3aa3c2d0451145167abd
PremaArjunan <153264236+PremaArjunan@users.noreply.github.com>
2023-12-12 03:36:46 +0530

    html code

---
+++ dom.js
@@ -0,0 +1,9 @@
+
+var headerTitle = document.getElementById('header-title');
+var header = document.getElementById('main-header');
+header.style.borderBottom = 'solid 3px #000';
+
+var header = document.querySelector('[data-container]');
+console.log(main);
+document.querySelector('[data-title]');
+main.style.backgroundcolor='green';
