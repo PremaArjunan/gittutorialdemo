7f3945c5d1b52b9873bd615bb5ac5d3d3b6e2112
PremaArjunan <153264236+PremaArjunan@users.noreply.github.com>
2023-12-12 03:56:19 +0530

    items in index.html

--- dom.js
+++ dom.js
@@ -1,9 +1,18 @@
 
-var headerTitle = document.getElementById('header-title');
-var header = document.getElementById('main-header');
-header.style.borderBottom = 'solid 3px #000';
+//var headerTitle = document.getElementById('header-title');
+///var header = document.getElementById('main-header');
+//header.style.borderBottom = 'solid 3px #000';
 
-var header = document.querySelector('[data-container]');
-console.log(main);
-document.querySelector('[data-title]');
-main.style.backgroundcolor='green';
+//var header = document.querySelector('[data-container]');
+//console.log(main);
+//document.querySelector('[data-title]');
+//main.style.backgroundcolor='green';
+
+var items = document.getElementsByClassName('list-group-item')
+console.log(items);
+console.log(items[0]);
+items[0].style.fontWeight = 'bold';
+items[1].style.fontWeight = 'bold';
+items[2].style.fontWeight = 'bold';
+items[3].style.fontWeight = 'bold';
+items[2].style.backgroundColor = 'green';
