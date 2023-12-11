784a9b9b3eab157838c36592333da8c3f1c56b92
PremaArjunan <153264236+PremaArjunan@users.noreply.github.com>
2023-12-12 04:45:13 +0530

    queryselector

--- dom.js
+++ dom.js
@@ -10,21 +10,40 @@
 
 
 //getElementsByClassName
-var items = document.getElementsByClassName('list-group-item')
-console.log(items);
-console.log(items[0]);
-items[0].textContent = 'hello';
-items[1].style.fontWeight = 'bold';
-items[2].style.fontWeight = 'bold';
-items[1].style.backgroundColor = 'green';
+//var items = document.getElementsByClassName('list-group-item')
+//console.log(items);
+//console.log(items[0]);
+//items[0].textContent = 'hello';
+//items[1].style.fontWeight = 'bold';
+//items[2].style.fontWeight = 'bold';
+//items[1].style.backgroundColor = 'green';
 
 
 
 //GETELEMENTBYTAGNAME
-var li = document.getElementsByTagName('li');
-console.log(li);
-console.log(li[1]);
-li[1].textContent = 'hello';
-li[1].style.fontWeight = 'bold';
-li[1].style.backgroundColor = 'green';
+//var li = document.getElementsByTagName('li');
+//console.log(li);
+//console.log(li[1]);
+//li[1].textContent = 'hello';
+//li[1].style.fontWeight = 'bold';
+//li[1].style.backgroundColor = 'green';
 
+
+//QUERYSELECTOR
+var header = document.querySelector('#main-header');
+header.computedStyleMap.borderBottoom = 'solid 4px #cc';
+
+var input = document.querySelector('input');
+input.value = 'Hello World';
+
+var submit = document.querySelector('input[type="submit"]');
+submit.value ="SEND";
+
+var item = document.querySelector('.list-group-item');
+item.style.color = 'red';
+
+var lastItem = document.querySelector('.list-group-item:last-child');
+lastItem.style.color = 'blue';
+
+var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.color = 'green';
