6cb6f26b3b58ede729c64b6742852d79b93c2aeb
PremaArjunan <153264236+PremaArjunan@users.noreply.github.com>
2023-12-12 05:20:35 +0530

    node and dom

--- dom.js
+++ dom.js
@@ -21,29 +21,46 @@
 
 
 //GETELEMENTBYTAGNAME
-//var li = document.getElementsByTagName('li');
+var li = document.getElementsByTagName('li');
 //console.log(li);
 //console.log(li[1]);
-//li[1].textContent = 'hello';
+li[0].textContent = 'hello';
 //li[1].style.fontWeight = 'bold';
 //li[1].style.backgroundColor = 'green';
 
 
 //QUERYSELECTOR
-var header = document.querySelector('#main-header');
-header.computedStyleMap.borderBottoom = 'solid 4px #cc';
+//var header = document.querySelector('#main-header');
+//header.computedStyleMap.borderBottoom = 'solid 4px #cc';
 
-var input = document.querySelector('input');
-input.value = 'Hello World';
+//var input = document.querySelector('input');
+//input.value = 'Hello World';
 
-var submit = document.querySelector('input[type="submit"]');
-submit.value ="SEND";
+//var submit = document.querySelector('input[type="submit"]');
+//submit.value ="SEND";
 
-var item = document.querySelector('.list-group-item');
-item.style.color = 'red';
+//var item = document.querySelector('.list-group-item');
+//item.style.color = 'red';
 
-var lastItem = document.querySelector('.list-group-item:last-child');
-lastItem.style.color = 'blue';
+//var lastItem = document.querySelector('.list-group-item:last-child');
+//lastItem.style.color = 'blue';
 
-var secondItem = document.querySelector('.list-group-item:nth-child(2)');
-secondItem.style.color = 'green';
+//var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+//secondItem.style.color = 'green';
+
+
+//QUERYSELECTORALL
+
+var titles = document.querySelectorAll('.titles');
+console.log(titles);
+//titles[0].textContent = 'Hello';
+
+var odd = document.querySelectorAll('li:nth-child(odd)');
+var even = document.querySelectorAll('li:nth-child(even)');
+for(var i=0; i < odd.length; i++)
+{
+    odd[i].style.backgroundColor = '#f4f4f4';
+    even[i].style.backgroundColor = '#ccc';
+}
+
+
