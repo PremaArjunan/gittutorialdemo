87d49adc144d3ca46a7e6b18e1317d6922ddee3d
PremaArjunan <153264236+PremaArjunan@users.noreply.github.com>
2024-02-04 02:58:51 +0530

    delete

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 function handleFormSubmit(event){
     event.preventDefault();
-  
-    const userDetails = {
-      username: event.target.username.value,
-      email:event.target.email.value,
-      phone:event.target.phone.value,
-    };
-    //localStorage.setItem("User Details", JSON.stringify(userDetails));
-    axios.get("https://crudcrud.com/api/f729d40a19b54fff9b887d339059d139/userDetails",userDetails)
+      let user = {
+          username:event.target.username.value,
+          email:event.target.email.value,
+          phone:event.target.phone.value
+      };
+      
+      axios.get("https://crudcrud.com/api/f729d40a19b54fff9b887d339059d139/userDetails",user)
       .then(res => console.log(res))
       .catch(err =>console.log(err))
-    }
-    //module.exports = handleFormSubmit;
+      //localStorage.setItem(user.email, JSON.stringify(user));
+      displayUsers(user);
+      }
+      
+  
+  
+      function displayUsers(user){
+        var userList = document.querySelector('ul');
+        let listItem = document.createElement('li');
+       listItem.appendChild(document.createTextNode(`${user.username}-${user.email}-${user.phone}`));
+        
+        let dele_button = document.createElement('button');
+        dele_button.appendChild(document.createTextNode('Delete'));
+          
+        listItem.appendChild(dele_button);
+        userList.appendChild(listItem);
+          
+        dele_button.addEventListener('click',function(event){
+            userList.removeChild(event.target.parentElement);
+            localStorage.removeItem(user.email);
+        }); 
+     }
