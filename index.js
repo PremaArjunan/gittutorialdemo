4ee75ed28b7c26c8c94647184fe25334454b48cd
PremaArjunan <153264236+PremaArjunan@users.noreply.github.com>
2024-02-04 02:47:11 +0530

    storeData

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
-function handleFormSubmit(event) {
-  event.preventDefault();
-
-  const user = event.target.username.value;
-  const email = event.target.email.value;
-  const phone = event.target.phone.value;
-
-
-  const newUser = {
-    username: user,
-    email: email,
-    phone: phone,
-  };
-
-  axios.post("http://crudcrud.com/api/1d261e1e4bb041609a596474a6464a58/appointmentData",newUser)
-  .then(res => console.log(res))
-  .catch(err =>console.log(err))
-}
+function handleFormSubmit(event){
+    event.preventDefault();
+  
+    const userDetails = {
+      username: event.target.username.value,
+      email:event.target.email.value,
+      phone:event.target.phone.value,
+    };
+    //localStorage.setItem("User Details", JSON.stringify(userDetails));
+    axios.get("https://crudcrud.com/api/f729d40a19b54fff9b887d339059d139/userDetails",userDetails)
+      .then(res => console.log(res))
+      .catch(err =>console.log(err))
+    }
+    //module.exports = handleFormSubmit;
